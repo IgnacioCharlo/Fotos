e7344b9897809da7deec469d91fb3f2123ebac06
[IgnacioCharlo] <[i.charlomillan@gmail.com]>
2019-11-13 20:34:15 +0100

    Some corrections

--- scripts.js
+++ scripts.js
@@ -1,25 +1,43 @@
-
 var img=document.getElementById('pic');
 
-var buttonNext = document.getElementById("b1");
-buttonNext.addEventListener('click', event=> {
-    movePhoto(1);
-});
-var buttonPrevious = document.getElementById("b2");
+var buttonPrevious = document.getElementById("b1");
 buttonPrevious.addEventListener('click', event=> {
     movePhoto(0);
 });
 
-count =0;  
-function movePhoto (derecha) {
+var buttonNext = document.getElementById("b2");
+buttonNext.addEventListener('click', event=> {
+    movePhoto(1);
+});
+
+
+//Direccion = 1 --> NEXT
+//Direccion = 0 --> PREVIOUS
+count =1;
+console.log("Fuera de la funcion, count =  " + count);
+function movePhoto (direccion) {
+    console.log("Dentro de la funcion, count =  " + count);
     var img=document.getElementById('pic');
-    if(derecha == 1){
+    if(direccion == 1){
         count++;
         img.src='cros'+count+'.jpeg';
+        console.log(count);
+        if(count === 5){
+            document.getElementById("b1");
+            inputbutton.disabled=false;
+        }
     }
-    else{
+    else if (direccion == 0){
         count--;
         img.src='cros'+count+'.jpeg';
+        console.log(count);
+        if (count === 1){
+            document.getElementById("b1");
+            inputbutton.disabled= false;
+        }
+    }
+    else{
+        console.log("ERROR con count");
     }
 }
 
