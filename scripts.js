af918eb37ff10493647a12e0cb6631a7f03455f3
Andrea Montoro Higueras <andrea.monhig@gmail.com>
2019-11-14 15:54:21 +0100

    fixed out of bounds error

--- scripts.js
+++ scripts.js
@@ -1,15 +1,34 @@
 var img=document.getElementById('pic');
-
+var inputbutton=document.getElementById('b1');
+var inputbutton=document.getElementById('b2');
 var buttonPrevious = document.getElementById("b1");
 buttonPrevious.addEventListener('click', event=> {
-    movePhoto(0);
+    disable_if_bound(0);
 });
 
 var buttonNext = document.getElementById("b2");
 buttonNext.addEventListener('click', event=> {
-    movePhoto(1);
+    disable_if_bound(1);
 });
 
+function disable_if_bound (direccion){
+    if(direccion === 1){
+        if(count === 5){
+            document.getElementById("b1");
+            inputbutton.disabled=false;
+            console.log(count);
+        }
+        else movePhoto(direccion);
+    }
+    else{
+        if (count === 1){
+            document.getElementById("b1");
+            inputbutton.disabled= false;
+            console.log(count);
+        }
+        else movePhoto(direccion);
+    }
+}
 
 //Direccion = 1 --> NEXT
 //Direccion = 0 --> PREVIOUS
@@ -19,22 +38,12 @@ function movePhoto (direccion) {
     console.log("Dentro de la funcion, count =  " + count);
     var img=document.getElementById('pic');
     if(direccion == 1){
-        count++;
-        img.src='cros'+count+'.jpeg';
-        console.log(count);
-        if(count === 5){
-            document.getElementById("b1");
-            inputbutton.disabled=false;
-        }
+        count++; 
+        img.src='cros'+count+'.jpeg';    
     }
     else if (direccion == 0){
         count--;
         img.src='cros'+count+'.jpeg';
-        console.log(count);
-        if (count === 1){
-            document.getElementById("b1");
-            inputbutton.disabled= false;
-        }
     }
     else{
         console.log("ERROR con count");
